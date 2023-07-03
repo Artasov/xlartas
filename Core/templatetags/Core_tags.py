dd9b7ea9b338687a98191618cf0f9a44834885a4
Nikita Artasov <ivanhvalevskey@gmail.com>
2023-07-03 08:15:52 +0300

    fix get_company_data custom tag in Core app

--- Core/templatetags/Core_tags.py
+++ Core/templatetags/Core_tags.py
@@ -19,7 +19,7 @@ def get_login_form():
 @register.simple_tag()
 def get_company_data(company_data_param: str):
     try:
-        return CompanyData.objects.get(name=company_data_param).value
+        return CompanyData.objects.get(param=company_data_param).value
     except CompanyData.DoesNotExist:
         return f'"{company_data_param}" not found in CompanyData.'
 
