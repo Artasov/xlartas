924863e485b931ac0aee0b5d003cde5fef2f9a06
xL <ivanhvalevskey@gmail.com>
2023-08-15 02:17:37 +0300

    fix orders_service.py

--- APP_shop/services/orders_service.py
+++ APP_shop/services/orders_service.py
@@ -3,6 +3,6 @@ from django.db.models import QuerySet
 from APP_shop.models import Order
 
 
-def get_user_orders(user_id: int) -> QuerySet[Order]:
+def get_user_orders(user_id: int):
     return Order.objects.filter(user_id=user_id) \
         .order_by('date_created')
