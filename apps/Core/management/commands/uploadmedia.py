ecbe5347b4525508ae633c13b6e8f976513f1453
Artasov <ivanhvalevskey@gmail.com>
2024-02-27 04:03:01 +0300

    process

--- apps/Core/management/commands/uploadmedia.py
+++ apps/Core/management/commands/uploadmedia.py
@@ -1,12 +1,15 @@
 import os
 
+from django.conf import settings
 from django.core.files import File
 from django.core.management.base import BaseCommand
-from django_minio_backend import MinioBackend  # Убедитесь, что это правильный импорт
+from django_minio_backend import MinioBackend
+from minio import Minio
+from minio.error import S3Error
 
 
 class Command(BaseCommand):
-    help = 'Uploads media files to the specified MinIO bucket'
+    help = 'Makes the media bucket public after uploading files'
 
     def add_arguments(self, parser):
         parser.add_argument('media_path', type=str, help='Path to the media folder')
@@ -17,18 +20,38 @@ class Command(BaseCommand):
             self.stdout.write(self.style.ERROR(f'{media_path} is not a valid directory'))
             return
 
-        # Создайте экземпляр MinioBackend здесь
+        # Убедитесь, что эти значения соответствуют вашей конфигурации
+        minio_client = Minio(
+            settings.MINIO_ENDPOINT,
+            access_key=settings.MINIO_ACCESS_KEY,
+            secret_key=settings.MINIO_SECRET_KEY,
+            secure=settings.MINIO_USE_HTTPS  # Используйте False, если ваш MinIO работает без SSL
+        )
+
+        bucket_name = "media"
         default_storage = MinioBackend()
 
         for root, dirs, files in os.walk(media_path):
             for file_name in files:
-                # Полный путь к файлу
                 file_path = os.path.join(root, file_name)
-                # Путь относительно папки media, который будет использоваться в MinIO
                 relative_path = os.path.relpath(file_path, media_path)
 
                 with open(file_path, 'rb') as file:
                     django_file = File(file)
-                    # Загружаем файл в MinIO, используя relative_path как путь в бакете
                     default_storage.save(relative_path, django_file)
                     self.stdout.write(self.style.SUCCESS(f'Successfully uploaded {relative_path}'))
+
+        try:
+            policy = '''{
+                "Version": "2012-10-17",
+                "Statement": [{
+                    "Effect": "Allow",
+                    "Principal": {"AWS": "*"},
+                    "Action": "s3:GetObject",
+                    "Resource": "arn:aws:s3:::%s/*"
+                }]
+            }''' % bucket_name
+            minio_client.set_bucket_policy(bucket_name, policy)
+            self.stdout.write(self.style.SUCCESS(f'Successfully set {bucket_name} bucket to public'))
+        except S3Error as e:
+            self.stdout.write(self.style.ERROR(f'Error setting bucket policy: {e}'))
