6525583aba42851b4040728ba75755f579840921
Artasov <ivanhvalevskey@gmail.com>
2024-03-03 23:09:09 +0300

    react process

---
+++ frontend/entrypoint_react.sh
@@ -0,0 +1,10 @@
+echo "#####################################"
+echo "######### React Starting... ########"
+echo "#####################################"
+
+echo "NPM INSTALL"
+npm install
+echo "NPM INSTALL END"
+echo "NPM RUN BUILD"
+npm run build
+echo "NPM RUN BUILD END"
