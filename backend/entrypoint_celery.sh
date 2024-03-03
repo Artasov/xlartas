e18f96bcbdbf17a3fa46d191a588d759307138cb
Artasov <ivanhvalevskey@gmail.com>
2024-03-03 20:39:49 +0300

    react process

--- backend/entrypoint_celery.sh
+++ backend/entrypoint_celery.sh
@@ -1,9 +1,14 @@
 #!/bin/sh
-
+echo "#####################################"
+echo "######### Celery Starting... ########"
+echo "#####################################"
 python manage.py migrate
-# Инициализация Django
-python -c "import os; os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'config.settings'); import django; django.setup()"
 
+# Инициализация Django
+# shellcheck disable=SC2164
+cd /srv/backend
+
+python -c "import os; os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'config.settings'); import django; django.setup()"
 echo "django inited for celery"
 
 sleep 1
