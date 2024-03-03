fc7f1a39203c1bfcc2f246a4c93b6f8b0d64dfa6
Artasov <ivanhvalevskey@gmail.com>
2024-03-04 01:24:54 +0300

    react process

--- entrypoint-minio.sh
+++ entrypoint-minio.sh
@@ -1,27 +1,37 @@
 #!/bin/sh
 # entrypoint-minio.sh
 
-# Запускаем сервер MinIO
 echo "#####################################"
-echo "######### Minio Starting... ########"
+echo "######### Minio Starting... #########"
 echo "#####################################"
+
+# Запускаем сервер MinIO в фоновом режиме
 minio server /data --console-address ":9001" &
 
-echo "wait minio starting..."
-sleep 10
+# Функция для проверки готовности MinIO
+check_minio_ready() {
+    mc alias set myminio http://localhost:9000 "${MINIO_ROOT_USER}" "${MINIO_ROOT_PASSWORD}"
+    mc ls myminio
+}
 
-echo "set alias"
-mc alias set myminio http://localhost:9000 "${MINIO_ROOT_USER}" "${MINIO_ROOT_PASSWORD}"
+# Ожидаем готовности MinIO
+until check_minio_ready; do
+  echo "Waiting for Minio to be ready..."
+  sleep 2
+done
+
+echo "Minio is ready."
 
 # Создаем бакет, если он не существует
-echo "Create media bucket if not exist"
+echo "Creating media bucket if not exist..."
 mc mb myminio/media --ignore-existing
 
-echo "Set public policy for media bucket"
+# Настраиваем публичную политику доступа к бакету
+echo "Setting public policy for media bucket..."
 mc policy set download myminio/media
+echo "Setting anonymous public policy for media bucket..."
+mc anonymous set download myminio/media
+echo "Minio setup complete."
 
-# Бесконечный цикл, чтобы контейнер не завершался
-echo "Minio start successfully"
-while true; do
-  sleep 60
-done
+# Блокируем скрипт, чтобы контейнер не завершал работу
+wait
