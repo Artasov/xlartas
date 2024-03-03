64547582f2710ce0b9499370e4dde8d2be1970f6
Artasov <ivanhvalevskey@gmail.com>
2024-03-04 00:55:06 +0300

    react process

--- entrypoint-minio.sh
+++ entrypoint-minio.sh
@@ -2,21 +2,26 @@
 # entrypoint-minio.sh
 
 # Запускаем сервер MinIO
+echo "#####################################"
+echo "######### Minio Starting... ########"
+echo "#####################################"
 minio server /data --console-address ":9001" &
 
-# Ждем запуска MinIO
+echo "wait minio starting..."
 sleep 10
 
-# Устанавливаем alias для MinIO
+echo "set alias"
 mc alias set myminio http://localhost:9000 "${MINIO_ROOT_USER}" "${MINIO_ROOT_PASSWORD}"
 
 # Создаем бакет, если он не существует
+echo "Create media bucket if not exist"
 mc mb myminio/media --ignore-existing
 
-# Делаем бакет публичным
+echo "Set public policy for media bucket"
 mc policy set download myminio/media
 
 # Бесконечный цикл, чтобы контейнер не завершался
+echo "Minio start successfully"
 while true; do
   sleep 60
 done
