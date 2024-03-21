1c90b90cf601799409af53f2402d5fa93b6a2a7b
Artasov <ivanhvalevskey@gmail.com>
2024-03-22 00:37:15 +0300

    react process

--- backend/apps/tinkoff/controllers/notify.py
+++ backend/apps/tinkoff/controllers/notify.py
@@ -1,3 +1,5 @@
+import logging
+
 from django.views.decorators.csrf import csrf_exempt
 from rest_framework.decorators import api_view, permission_classes
 from rest_framework.permissions import AllowAny
