212ff3904179dc2a8875ecec24660e90c9047bfc
Artasov <ivanhvalevskey@gmail.com>
2024-03-22 04:26:20 +0300

    react process

--- backend/apps/shop/services/orders.py
+++ backend/apps/shop/services/orders.py
@@ -17,13 +17,12 @@ def get_user_orders(user_id: int) -> QuerySet[SoftwareSubscriptionOrder]:
 
 
 async def execute_tinkoff_deposit_order(order: TinkoffDepositOrder):
-    user = await sync_to_async(getattr)(order, 'user', None)
+    order.user = await sync_to_async(getattr)(order, 'user', None)
     if order.is_paid:
         raise SomethingGoWrong
-    user.balance += order.amount
+    order.user.balance += order.amount
     order.is_paid = True
     await order.asave()
-    await user.asave()
 
 
 async def execute_order_by_id(order_id: str):
