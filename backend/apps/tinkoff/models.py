ee207f3a58ab3f5d89526731a95cfb1fe4ddf615
Artasov <ivanhvalevskey@gmail.com>
2024-03-23 09:02:05 +0300

    react process

--- backend/apps/tinkoff/models.py
+++ backend/apps/tinkoff/models.py
@@ -1,10 +1,12 @@
 from django.db.models import Model
+from django.forms import CharField
 
 from apps.Core.models.user import User
 from apps.shop.models import BaseOrder
 
 
 class TinkoffDepositOrder(BaseOrder):
+    order_id = CharField(max_length=100, unique=True, default=uuid.uuid4, editable=False)
 
     def __str__(self):
         return f"Order #{self.id}"
