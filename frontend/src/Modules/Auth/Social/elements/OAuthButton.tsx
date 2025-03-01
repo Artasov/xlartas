37e0dfc350f0ab4bb57c7a806c9f0d085241e5f9
Artasov <ivanhvalevskey@gmail.com>
2025-03-01 04:25:44 +0300

    process

--- frontend/src/Modules/Auth/Social/elements/OAuthButton.tsx
+++ frontend/src/Modules/Auth/Social/elements/OAuthButton.tsx
@@ -1,11 +1,12 @@
 // Modules/Auth/Social/elements/OAuthButton.tsx
-import React from "react";
+import React, {useEffect} from "react";
 import { IconButton } from "@mui/material";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
-import { DOMAIN_URL_ENCODED } from "../../../Api/axiosConfig";
+import {DOMAIN_URL, DOMAIN_URL_ENCODED} from "../../../Api/axiosConfig";
 import { OAuthProvider } from "Auth/Social/types";
 import CheckCircleIcon from '@mui/icons-material/CheckCircle';
 import { useTheme } from "Theme/ThemeContext";
+import pprint from "Utils/pprint";
 
 interface OAuthButtonProps {
     provider: OAuthProvider;
@@ -35,6 +36,11 @@ const OAuthButton: React.FC<OAuthButtonProps> = (
     const { theme } = useTheme();
     const oauthUrl = `${redirectUri}?response_type=code&client_id=${clientId}&redirect_uri=${DOMAIN_URL_ENCODED}%2Foauth%2F${provider}%2Fcallback%2F&scope=${scope}&state=${nextParam}&next=${nextFromUrl}`;
     const iconStyle = { color: theme.palette.text.primary80, height: `${pxIconSize}px` };
+
+    useEffect(() => {
+        pprint(`Provider ${provider}`)
+        pprint(`redirectUri: ${DOMAIN_URL}/oauth/${provider}/callback/`)
+    }, []);
 
     return (
         <div style={{ position: 'relative', display: 'inline-block' }}>
