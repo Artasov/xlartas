134c084d049da98c203cb8c914779dc261e02f32
Artasov <ivanhvalevskey@gmail.com>
2025-03-09 12:41:40 +0300

    process

--- backend/apps/core/tasks/test_tasks.py
+++ backend/apps/core/tasks/test_tasks.py
@@ -2,6 +2,7 @@
 import logging
 from time import sleep
 
+from adjango.decorators import task
 from celery import shared_task
 from celery_singleton import Singleton
 from django.conf import settings
@@ -9,7 +10,16 @@ from django.conf import settings
 log = logging.getLogger('console')
 
 
+@shared_task
+@task('global')
+def test_task(rand_integer):
+    sleep(5)
+    if settings.INTENSIVE_HEALTH_TEST:
+        log.info(f'TASK SUCCESS {rand_integer}')
+
+
 @shared_task(base=Singleton, unique_on=['rand_integer', ])
+@task('global')
 def test_task(rand_integer):
     sleep(5)
     if settings.INTENSIVE_HEALTH_TEST:
@@ -17,6 +27,7 @@ def test_task(rand_integer):
 
 
 @shared_task
+@task('global')
 def test_periodic_task(param1):
     if settings.INTENSIVE_HEALTH_TEST:
         log.info(f'TASK PERIODIC SUCCESS {param1}')
