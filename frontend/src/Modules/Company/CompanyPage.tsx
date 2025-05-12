f8013a451e9f52db33d6563df7951ba26b7f87cc
Artasov <ivanhvalevskey@gmail.com>
2025-05-12 15:45:09 +0300

    feat: design CompanyPage.tsx

--- frontend/src/Modules/Company/CompanyPage.tsx
+++ frontend/src/Modules/Company/CompanyPage.tsx
@@ -7,6 +7,8 @@ import {FC, FCSS, FR} from "WideLayout/Layouts";
 import {Company} from "Company/Types";
 import {useTheme} from "Theme/ThemeContext";
 import {useApi} from "../Api/useApi";
+import copyToClipboard from "Utils/clipboard";
+import './CompanyPage.sass'
 
 
 const CompanyPage: React.FC = () => {
@@ -14,7 +16,7 @@ const CompanyPage: React.FC = () => {
     const [company, setCompany] = useState<Company | null>(null);
     const [loading, setLoading] = useState<boolean>(true);
     const navigate = useNavigate();
-    const {plt, theme} = useTheme();
+    const {theme} = useTheme();
     const {api} = useApi();
 
     useEffect(() => {
@@ -23,7 +25,7 @@ const CompanyPage: React.FC = () => {
             api.get(`/api/v1/companies/${encodeURIComponent(name)}/`).then(data => setCompany(data)).finally(() => setLoading(false));
         };
         if (name) {
-            fetchCompany();
+            fetchCompany().then();
         } else {
             setLoading(false);
             Message.error('Название компании не указано.');
@@ -39,17 +41,53 @@ const CompanyPage: React.FC = () => {
     }
 
     return (
-        <FCSS lh={'1.3rem'} px={2} g={1} maxW={400} mx={'auto'}>
+        <FCSS lh={'1.3rem'} px={2} g={1} maxW={500} mx={'auto'}>
             <h1 style={{lineHeight: '1.6rem'}} className={'fs-4'}>{company.name}</h1>
             <h2 style={{lineHeight: '1.4rem'}} className={'fs-5'}>Общее</h2>
             <FC pl={2}>
-                <FR>ИП: {company.person_name}</FR>
-                <FR>Адрес: {company.address}</FR>
-                <FR>ОГРН: {company.ogrn}</FR>
-                <FR>ИНН: {company.inn}</FR>
-                <FR>БИК банка: {company.bik}</FR>
-                <FR>Корреспондентский счет: {company.current_account}</FR>
-                <FR>Расчетный счет: {company.checking_account}</FR>
+                {typeof company.person_name === 'string' &&
+                    <span>
+                        <span>ИП: </span>
+                        <span className={'cred-span'} onClick={
+                            () => copyToClipboard(String(company.person_name))
+                        }>{company.person_name}</span>
+                    </span>}
+                <span>
+                    <span>Адрес: </span>
+                    <span className={'cred-span'} onClick={
+                        () => copyToClipboard(company.address)
+                    }>{company.address}</span>
+                </span>
+                <span>
+                    <span>ОГРН: </span>
+                    <span className={'cred-span'} onClick={
+                        () => copyToClipboard(company.ogrn)
+                    }>{company.ogrn}</span>
+                </span>
+                <span>
+                    <span>ИНН: </span>
+                    <span className={'cred-span'} onClick={
+                        () => copyToClipboard(company.inn)
+                    }>{company.inn}</span>
+                </span>
+                <span>
+                    <span>БИК банка: </span>
+                    <span className={'cred-span'} onClick={
+                        () => copyToClipboard(company.bik)
+                    }>{company.bik}</span>
+                </span>
+                <span>
+                    <span>Корреспондентский счет: </span>
+                    <span className={'cred-span'} onClick={
+                        () => copyToClipboard(company.current_account)
+                    }>{company.current_account}</span>
+                </span>
+                <span>
+                    <span>Расчетный счет: </span>
+                    <span className={'cred-span'} onClick={
+                        () => copyToClipboard(company.checking_account)
+                    }>{company.checking_account}</span>
+                </span>
             </FC>
             {company.documents && company.documents.length > 0
                 ? <>
