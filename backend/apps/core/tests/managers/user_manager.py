61f636ef60418703d23fbc7bfc77f3f4f554c387
Artasov <71753135+Artasov@users.noreply.github.com>
2025-06-06 06:30:33 +0300

    Add user manager credentials test (#5)

--- backend/apps/core/tests/managers/user_manager.py
+++ backend/apps/core/tests/managers/user_manager.py
@@ -1,13 +1,20 @@
 # core/tests/managers/user_manager.py
 import pytest
 
+from apps.core.models import User
+
 
 @pytest.mark.django_db
 @pytest.mark.asyncio
 async def test_user_by_creds():
-    # user = await UserFactory.acreate()
-    # email_user = await User.objects.by_creds(credential=user.email)
-    # phone_user = await User.objects.by_creds(credential=str(user.phone))
-    # assert email_user == user
-    # assert phone_user == user
-    pass  # TODO не верно
+    user = await User.objects.acreate(
+        username="testuser",
+        email="test@example.com",
+        phone="+71234567890",
+    )
+
+    email_user = await User.objects.by_creds(credential=user.email)
+    phone_user = await User.objects.by_creds(credential=str(user.phone))
+
+    assert email_user == user
+    assert phone_user == user
