0bdc754d8e4736ced0b9a01854ca9c4db9ebf75a
Artasov <71753135+Artasov@users.noreply.github.com>
2025-07-01 14:13:34 +0300

    Add missing translations (#35)

--- backend/apps/surveys/exceptions/base.py
+++ backend/apps/surveys/exceptions/base.py
@@ -1,27 +1,28 @@
 # surveys/exceptions/base.py
 from rest_framework import status
 from rest_framework.exceptions import APIException
+from django.utils.translation import gettext_lazy as _
 
 
 class CurrentUserNotSurveyAuthor(APIException):
     status_code = status.HTTP_403_FORBIDDEN
-    default_detail = {'message': 'You are not the author of the survey.'}
+    default_detail = {'message': _('You are not the author of the survey.')}
     default_code = 'current_user_not_survey_author'
 
 
 class SurveyIdWasNotProvided(APIException):
     status_code = status.HTTP_400_BAD_REQUEST
-    default_detail = {'message': 'Survey id was not provided.'}
+    default_detail = {'message': _('Survey id was not provided.')}
     default_code = 'survey_id_was_not_provided'
 
 
 class ChangeCompletedSurveyAttemptForbidden(APIException):
     status_code = status.HTTP_423_LOCKED
-    default_detail = {'message': 'Нельзя изменять уже законченную попытку.'}
+    default_detail = {'message': _('Нельзя изменять уже законченную попытку.')}
     default_code = 'change_completed_survey_attempt_forbidden'
 
 
 class HaveNoMoreAttemptsLeftToPass(APIException):
     status_code = status.HTTP_403_FORBIDDEN
-    default_detail = {'message': 'У вас не осталось попыток для прохождения.'}
+    default_detail = {'message': _('У вас не осталось попыток для прохождения.')}
     default_code = 'have_no_more_attempts_left_to_pass'
