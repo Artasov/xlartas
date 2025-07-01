d256ac020874713d2fc72b0b20391e87ab774286
Artasov <ivanhvalevskey@gmail.com>
2025-07-01 14:50:15 +0300

    process

--- frontend/src/Locale/ruMap.ts
+++
@@ -1,7 +0,0 @@
-// Locale/ruMap.ts
-export const orderStatusMapping: Record<string, string> = {
-    'new': 'Не оплачен',
-    'paid': 'Оплачен',
-    'executed': 'Оплачен',
-    'cancelled': 'Отменён',
-};
