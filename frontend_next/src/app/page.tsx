d7b74aca1638042a363668df2fdb71251119dc4f
Artasov <ivanhvalevskey@gmail.com>
2025-07-28 10:35:35 +0300

    process

--- frontend_next/src/app/page.tsx
+++ frontend_next/src/app/page.tsx
@@ -1,9 +1,4 @@
-"use client";
-
 import dynamic from "next/dynamic";
-
-// Enable server-side rendering for the RootApp component by removing
-// the `ssr: false` option. Dynamic imports default to SSR enabled.
 const RootApp = dynamic(() => import("../App"));
 
 export default function Home() {
