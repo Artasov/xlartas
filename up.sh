b99af1a10899174c6c6cdc22cb7765e4a020d5d1
Artasov <ivanhvalevskey@gmail.com>
2025-12-15 16:08:52 +0300

    process

--- up.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-echo "------------------------------------------"
-echo "Starting containers with Docker Compose..."
-echo "------------------------------------------"
-docker-compose --env-file ./backend/.env -f docker-compose.yml up
